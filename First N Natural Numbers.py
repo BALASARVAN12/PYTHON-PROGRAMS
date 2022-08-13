e5f485a7f16a2880b2030baa213416ae08b2c3ab
BALASARVAN12 <91976828+BALASARVAN12@users.noreply.github.com>
2022-08-13 13:15:05 +0530

    Create First N Natural Numbers.py

---
+++ First N Natural Numbers.py
@@ -0,0 +1,11 @@
+#USING WHILE LOOP
+num=int(input("ENTER A NUMBER:"))
+i=1
+while i<=num:
+  print(i,end=" ")
+  i+=1
+  
+#USING FOR LOOP
+num=int(input("ENTER A NUMBER:"))
+for i in range(1,num+1):
+  print(i,end=" ")
